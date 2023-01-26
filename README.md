584aacb80ba7ac222fa364722ac7c802019f3dd6
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2023-01-26 21:06:33 +0200

    - Initial release

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# TinyPNG
-Lossy optimization for any PNG, JPEG, APNG, and WEBP images by TinyPNG service.
+![GitHub](https://img.shields.io/github/license/viacheslav-lozinskyi/TinyPNG)
+[![GitHub issues](https://img.shields.io/github/issues/viacheslav-lozinskyi/TinyPNG)](https://github.com/viacheslav-lozinskyi/TinyPNG/issues)
+[![GitHub Repo stars](https://img.shields.io/github/stars/viacheslav-lozinskyi/TinyPNG)](https://github.com/viacheslav-lozinskyi/TinyPNG/stargazers)
+[![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/viacheslav-lozinskyi/TinyPNG)](https://github.com/viacheslav-lozinskyi/TinyPNG)
+[![Image](https://img.shields.io/badge/VS-2022-blueviolet)](https://marketplace.visualstudio.com/items?itemName=ViacheslavLozinskyi.MetaOutput-2022)
+[![Image](https://img.shields.io/badge/VS-2019-blueviolet)](https://marketplace.visualstudio.com/items?itemName=ViacheslavLozinskyi.MetaOutput-2019)
+[![Image](https://img.shields.io/badge/VS-2017-blueviolet)](https://marketplace.visualstudio.com/items?itemName=ViacheslavLozinskyi.MetaOutput-2019)
+
+---
+
+## Important information:
+[![MetaOutput](https://www.metaoutput.net/_functions/watch?utm_source=github.com&utm_medium=referral&utm_campaign=view-on-github&utm_content=TinyPNG&source=GITHUB&size=128x128&project=TinyPNG&url=https://github.com/viacheslav-lozinskyi/TinyPNG)](https://www.metaoutput.net/)
+
+This extension is working only with installed [MetaOutput](https://www.metaoutput.net/) extension!
+
+---
+
+## Description:
+**TinyPNG** uses smart lossy compression techniques to reduce the file size of your **WEBP**, **JPEG** and **PNG** files.
+By selectively decreasing the number of colors in the image, fewer bytes are required to store the data.
+The effect is nearly invisible, but it makes a very large difference in file size!
+
+Detailed information about this service is [HERE](https://tinypng.com/).
+
+---
+
+## Free usage:
+This extension is free, but the service which does this optimization isn't free.
+It provides compression for 500 free images in month.
+Other conversions will be necessary to purchase.
+Also, this service is working only with "Developer API key".
+It's necessary to get [HERE](https://tinypng.com/developers).
+
+Good news that this service provides the best compression of all alternatives.
+Therefore, it makes sense...
+
+---
+
+## How it works:
+![Image](https://viacheslav-lozinskyi.github.io/TinyPNG/resource/video/Presentation1.gif)
+
+---
+
+## Prerequisites:
+- **Visual Studio** 2017, 2019, 2022
+- **.NET Framework** 4.7.2+
+- **MetaOutput** extension
+- **Internet connection**
+
+---
+
+## Dependencies:
+- Data visualization is based on [MetaOutput](https://www.metaoutput.net/)
+- Data compression is based on [Tinify](https://www.nuget.org/packages/Tinify)
+
+---
+
+## How can I help:
+- If you like using this extension, please give it a ★★★★★ rating on the [Visual Studio Marketplace](https://marketplace.visualstudio.com/items?itemName=ViacheslavLozinskyi.TinyPNG&ssr=false#review-details).
+- If you found bugs or you have feature requests, please write it on [GitHub](https://github.com/viacheslav-lozinskyi/TinyPNG).
---
+++ application/tinypng.vs/resource/package/VSPackage.cs
@@ -0,0 +1,81 @@
+﻿
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace resource.package
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(CONSTANT.GUID)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.ShellInitialized_string, PackageAutoLoadFlags.BackgroundLoad)]
+    public sealed class PreviewAUDIO : AsyncPackage
+    {
+        internal static class CONSTANT
+        {
+            public const string APPLICATION = "Visual Studio";
+            public const string COPYRIGHT = "Copyright (c) 2020-2023 by Viacheslav Lozinskyi. All rights reserved.";
+            public const string DESCRIPTION = "Lossy optimization for any PNG, JPEG, APNG, and WEBP images by TinyPNG service.";
+            public const string GUID = "5CF22EE9-9ECF-4A6C-BF4C-20981DA5095A";
+            public const string NAME = "TinyPNG";
+            public const string VERSION = "1.0.1";
+        }
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            {
+                extension.AnyPreview.Connect();
+            }
+            {
+                await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            }
+            try
+            {
+                //if (string.IsNullOrEmpty(atom.Trace.GetFailState(CONSTANT.APPLICATION)) == false)
+                //{
+                //    var a_Context = Package.GetGlobalService(typeof(SDTE)) as DTE2;
+                //    if (a_Context != null)
+                //    {
+                //        var a_Context1 = (OutputWindowPane)null;
+                //        for (var i = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Count; i >= 1; i--)
+                //        {
+                //            if (a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i).Name == "MetaOutput")
+                //            {
+                //                a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i);
+                //                break;
+                //            }
+                //        }
+                //        if (a_Context1 == null)
+                //        {
+                //            a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Add("MetaOutput");
+                //        }
+                //        if (a_Context1 != null)
+                //        {
+                //            a_Context1.OutputString("\r\n" + CONSTANT.NAME + " extension doesn't work without MetaOutput.\r\n    Please install it (https://www.metaoutput.net/download)\r\n");
+                //            a_Context1.Activate();
+                //        }
+                //    }
+                //}
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        protected override int QueryClose(out bool canClose)
+        {
+            {
+                extension.AnyPreview.Disconnect();
+                canClose = true;
+            }
+            return VSConstants.S_OK;
+        }
+    }
+}
