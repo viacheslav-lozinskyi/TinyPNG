13d186a789b17b2fb0e60d13b8b19d36bdd6ccfa
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2023-02-03 00:55:02 +0200

    - Visual part of the extension is implemented

--- application/tinypng.vs/properties/AssemblyInfo.cs
+++ application/tinypng.vs/properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle(resource.package.PreviewAUDIO.CONSTANT.NAME)]
-[assembly: AssemblyDescription(resource.package.PreviewAUDIO.CONSTANT.DESCRIPTION)]
+[assembly: AssemblyTitle(resource.package.TinyPNG.CONSTANT.NAME)]
+[assembly: AssemblyDescription(resource.package.TinyPNG.CONSTANT.DESCRIPTION)]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct(resource.package.PreviewAUDIO.CONSTANT.NAME)]
-[assembly: AssemblyCopyright(resource.package.PreviewAUDIO.CONSTANT.COPYRIGHT)]
+[assembly: AssemblyProduct(resource.package.TinyPNG.CONSTANT.NAME)]
+[assembly: AssemblyCopyright(resource.package.TinyPNG.CONSTANT.COPYRIGHT)]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion(resource.package.PreviewAUDIO.CONSTANT.VERSION)]
-[assembly: AssemblyFileVersion(resource.package.PreviewAUDIO.CONSTANT.VERSION)]
+[assembly: AssemblyVersion(resource.package.TinyPNG.CONSTANT.VERSION)]
+[assembly: AssemblyFileVersion(resource.package.TinyPNG.CONSTANT.VERSION)]
--- application/tinypng.vs/resource/package/VSPackage.cs
+++ application/tinypng.vs/resource/package/VSPackage.cs
@@ -15,53 +15,59 @@ namespace resource.package
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(CONSTANT.GUID)]
     [ProvideAutoLoad(VSConstants.UICONTEXT.ShellInitialized_string, PackageAutoLoadFlags.BackgroundLoad)]
-    public sealed class PreviewAUDIO : AsyncPackage
+    public sealed class TinyPNG : AsyncPackage
     {
         internal static class CONSTANT
         {
             public const string APPLICATION = "Visual Studio";
             public const string COPYRIGHT = "Copyright (c) 2020-2023 by Viacheslav Lozinskyi. All rights reserved.";
             public const string DESCRIPTION = "Lossy optimization for any PNG, JPEG, APNG, and WEBP images by TinyPNG service.";
-            public const string GUID = "5CF22EE9-9ECF-4A6C-BF4C-20981DA5095A";
+            public const string GUID = "68C2787B-4F41-4145-BD12-17BEC8832539";
             public const string NAME = "TinyPNG";
             public const string VERSION = "1.0.1";
         }
 
+        internal static AsyncPackage Instance { get; private set; }
+
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             {
-                extension.AnyPreview.Connect();
+                Instance = this;
             }
             {
                 await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
             }
             try
             {
-                //if (string.IsNullOrEmpty(atom.Trace.GetFailState(CONSTANT.APPLICATION)) == false)
-                //{
-                //    var a_Context = Package.GetGlobalService(typeof(SDTE)) as DTE2;
-                //    if (a_Context != null)
-                //    {
-                //        var a_Context1 = (OutputWindowPane)null;
-                //        for (var i = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Count; i >= 1; i--)
-                //        {
-                //            if (a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i).Name == "MetaOutput")
-                //            {
-                //                a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i);
-                //                break;
-                //            }
-                //        }
-                //        if (a_Context1 == null)
-                //        {
-                //            a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Add("MetaOutput");
-                //        }
-                //        if (a_Context1 != null)
-                //        {
-                //            a_Context1.OutputString("\r\n" + CONSTANT.NAME + " extension doesn't work without MetaOutput.\r\n    Please install it (https://www.metaoutput.net/download)\r\n");
-                //            a_Context1.Activate();
-                //        }
-                //    }
-                //}
+                if (string.IsNullOrEmpty(atom.Trace.GetFailState(CONSTANT.APPLICATION)) == false)
+                {
+                    var a_Context = Package.GetGlobalService(typeof(SDTE)) as DTE2;
+                    if (a_Context != null)
+                    {
+                        var a_Context1 = (OutputWindowPane)null;
+                        for (var i = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Count; i >= 1; i--)
+                        {
+                            if (a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i).Name == "MetaOutput")
+                            {
+                                a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i);
+                                break;
+                            }
+                        }
+                        if (a_Context1 == null)
+                        {
+                            a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Add("MetaOutput");
+                        }
+                        if (a_Context1 != null)
+                        {
+                            a_Context1.OutputString("\r\n" + CONSTANT.NAME + " extension doesn't work without MetaOutput.\r\n    Please install it (https://www.metaoutput.net/download)\r\n");
+                            a_Context1.Activate();
+                        }
+                    }
+                }
+                else
+                {
+                    tool.VSTool.Connect();
+                }
             }
             catch (Exception ex)
             {
@@ -72,7 +78,7 @@ namespace resource.package
         protected override int QueryClose(out bool canClose)
         {
             {
-                extension.AnyPreview.Disconnect();
+                tool.VSTool.Disconnect();
                 canClose = true;
             }
             return VSConstants.S_OK;
