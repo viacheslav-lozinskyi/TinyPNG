5ca498bc06d5af900a88a12ee6c99a24056217e0
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2023-02-08 20:20:30 +0200

    - Minor improvements

---
+++ application/tinypng.vs/resource/pipe/VSPipe.cs
@@ -0,0 +1,11 @@
+using atom;
+
+namespace resource.pipe
+{
+    internal class VSPipe : extension.AnyPipe
+    {
+        protected override void _Execute(Trace context, string name, string value)
+        {
+        }
+    }
+}
