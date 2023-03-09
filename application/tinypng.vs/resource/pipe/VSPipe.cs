46b7873becf4b38277f1bd990d22eef99d6b499c
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2023-03-09 21:25:22 +0200

    - Correct visualization of this tool is implemented

--- application/tinypng.vs/resource/pipe/VSPipe.cs
+++ application/tinypng.vs/resource/pipe/VSPipe.cs
@@ -1,11 +1,19 @@
-using atom;
+
+using Microsoft.VisualStudio.Shell;
+using resource.tool;
 
 namespace resource.pipe
 {
     internal class VSPipe : extension.AnyPipe
     {
-        protected override void _Execute(Trace context, string name, string value)
+        protected override void _Execute(atom.Trace context, string pipe, string value)
         {
+            //var a_Name1 = GetAttribute(value, NAME.ATTRIBUTE.EVENT);
+            //var a_Name2 = GetAttribute(value, NAME.ATTRIBUTE.ID);
+            ThreadHelper.ThrowIfNotOnUIThread();
+            {
+                package.TinyPNG.Instance.ShowOptionPage(typeof(VSOptions));
+            }
         }
     }
 }
